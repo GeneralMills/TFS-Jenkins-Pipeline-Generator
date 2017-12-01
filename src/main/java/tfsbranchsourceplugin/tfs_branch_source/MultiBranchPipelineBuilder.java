250d3d7d96df70f9a3ab23111a398846588cf6ea
EricBartusch <ericbartusch8@gmail.com>
2017-12-01 14:38:40 -0600

    Error the build if team project folder doesn't exist.  Bring back code that is actually needed.

--- src/main/java/tfsbranchsourceplugin/tfs_branch_source/MultiBranchPipelineBuilder.java
+++ src/main/java/tfsbranchsourceplugin/tfs_branch_source/MultiBranchPipelineBuilder.java
@@ -10,6 +10,7 @@ import com.squareup.okhttp.Authenticator;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
+import hudson.AbortException;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
@@ -22,13 +23,12 @@ import hudson.util.ListBoxModel;
 import jenkins.model.Jenkins;
 import jenkins.tasks.SimpleBuildStep;
 import net.sf.json.JSONObject;
-import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject;
+import org.jenkinsci.plugins.gitclient.GitClient;
 import org.json.JSONArray;
 import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
-import org.jenkinsci.plugins.gitclient.GitClient;
 
 import java.io.*;
 import java.net.Proxy;
@@ -43,37 +43,26 @@ public class MultiBranchPipelineBuilder extends Builder implements SimpleBuildSt
     private final String projectRecognizer;
 
 
-    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
     @DataBoundConstructor
-    public MultiBranchPipelineBuilder(String teamProjectUrl, String teamProject, String credentialsId, String projectRecognizer) {
+    public MultiBranchPipelineBuilder(String teamProjectUrl, String credentialsId, String projectRecognizer) {
         this.teamProjectUrl = teamProjectUrl;
         this.credentialsId = credentialsId;
         this.projectRecognizer = projectRecognizer;
 
     }
 
-    /**
-     * We'll use this from the {@code config.jelly}.
-     */
-    public String getTeamProjectUrl() {
-        return teamProjectUrl;
-    }
     public String getCredentialsId() {
         return credentialsId;
     }
-    public String getProjectRecognizer() {
-        return projectRecognizer;
-    }
-
 
     @Override
-    public void perform(Run<?,?> build, FilePath workspace, Launcher launcher, TaskListener listener) throws IOException, InterruptedException {
+    public void perform(Run<?, ?> build, FilePath workspace, Launcher launcher, TaskListener listener) throws IOException, InterruptedException {
 
         String url = this.teamProjectUrl;
         String file = projectRecognizer;
         String credentials = credentialsId;
 
-        if(url.endsWith("/")) {
+        if (url.endsWith("/")) {
             url = url.substring(0, url.length() - 1);
         }
 
@@ -81,11 +70,9 @@ public class MultiBranchPipelineBuilder extends Builder implements SimpleBuildSt
         String team = url.substring(url.lastIndexOf('/') + 1);
 
         final StandardUsernamePasswordCredentials tfsCredentials;
-        try
-        {
+        try {
             tfsCredentials = getStandardUsernamePasswordCredentials(credentials);
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             listener.getLogger().println(e.getMessage());
             return;
         }
@@ -96,15 +83,12 @@ public class MultiBranchPipelineBuilder extends Builder implements SimpleBuildSt
         listener.getLogger().println("--Looking through Team Project for repos--");
         JSONArray repos = GetReposForTeamProject(listener, client, url);
 
-        for(int i = 0; i < repos.length(); i++)
-        {
+        for (int i = 0; i < repos.length(); i++) {
             listener.getLogger().println("\n\n--Looking through repo for branches--");
             JSONArray branches = GetBranchesForRepo(listener, client, url, repos.getJSONObject(i));
 
-            for(int j = 0; j < branches.length(); j++)
-            {
-                if(checkBranchesForFile(listener, client, url, repos.getJSONObject(i).get("id").toString(), branches.getJSONObject(j), file))
-                {
+            for (int j = 0; j < branches.length(); j++) {
+                if (checkBranchesForFile(listener, client, url, repos.getJSONObject(i).get("id").toString(), branches.getJSONObject(j), file)) {
                     //Once we have found the file we are looking for, we don't need to check the rest of the branches
                     reposWithFile.add(repos.getJSONObject(i).get("name").toString());
                     break;
@@ -112,31 +96,35 @@ public class MultiBranchPipelineBuilder extends Builder implements SimpleBuildSt
             }
         }
 
-        listener.getLogger().println("\n\n--Repos with the file--");
+        listener.getLogger().println("\n\n--Processing repos--");
         Folder folder = Jenkins.getInstance().getItemByFullName(team, Folder.class);
-        for(String name : reposWithFile)
-        {
-            File xmlFile = new File("C:\\Projects\\GMITFSPlugin\\tfs_vsts_branch_source\\xml\\config.xml");
-            listener.getLogger().println(name);
-            InputStream foobar = replaceTokensInXML(xmlFile, name, credentials, url, file, team);
-            TopLevelItem job = Jenkins.getInstance().getItemByFullName(name, WorkflowMultiBranchProject.class);
-            if(job == null)
-            {
-                folder.createProjectFromXML(name, foobar);
-            }
-            else
-            {
-                listener.getLogger().println(name + "already exists");
+        if (folder == null) {
+            throw new AbortException("Team project folder does not exist!  Please make a folder for the " + team + " team project before running this job.");
+        } else {
+            for (String name : reposWithFile) {
+                File xmlFile = new File("C:\\Projects\\GMITFSPlugin\\tfs_vsts_branch_source\\xml\\config.xml");
+                InputStream foobar = replaceTokensInXML(xmlFile, name, credentials, url, file, team);
+                TopLevelItem job = folder.getItem(name);
+                if (job == null) {
+                    folder.createProjectFromXML(name, foobar);
+                    listener.getLogger().println("Created multibranch pipeline for: " + name);
+                } else {
+                    listener.getLogger().println(name + " multibranch pipeline already exists");
+                }
             }
         }
+    }
+
+    @Override
+    public MultiBranchPipelineBuilder.DescriptorImpl getDescriptor() {
+        return (MultiBranchPipelineBuilder.DescriptorImpl) super.getDescriptor();
     }
 
     private JSONArray GetReposForTeamProject(TaskListener listener, OkHttpClient client, String teamProjectUrl) throws IOException {
         String listOfReposUrl = teamProjectUrl + "/_apis/git/repositories?api-version=1";
         org.json.JSONObject obj = callGet(client, listOfReposUrl);
         JSONArray repos = obj.getJSONArray("value");
-        for(int i = 0; i < repos.length(); i++)
-        {
+        for (int i = 0; i < repos.length(); i++) {
             org.json.JSONObject repo = repos.getJSONObject(i);
             listener.getLogger().println("Found repo: - " + repo.get("id") + " - " + repo.get("name"));
         }
@@ -144,12 +132,11 @@ public class MultiBranchPipelineBuilder extends Builder implements SimpleBuildSt
     }
 
     private JSONArray GetBranchesForRepo(TaskListener listener, OkHttpClient client, String teamProjectUrl, org.json.JSONObject repo) throws IOException {
-        String listOfBranchesUrl = teamProjectUrl + "/_apis/git/repositories/"+ repo.get("id") +"/refs?filter=heads&api-version=1.0";
+        String listOfBranchesUrl = teamProjectUrl + "/_apis/git/repositories/" + repo.get("id") + "/refs?filter=heads&api-version=1.0";
         org.json.JSONObject obj = callGet(client, listOfBranchesUrl);
         JSONArray branches = obj.getJSONArray("value");
         listener.getLogger().println("Repo: " + repo.get("name"));
-        for(int j = 0; j < branches.length(); j++)
-        {
+        for (int j = 0; j < branches.length(); j++) {
             org.json.JSONObject branch = branches.getJSONObject(j);
             listener.getLogger().println("Found branch: " + branch.get("name"));
         }
@@ -162,14 +149,13 @@ public class MultiBranchPipelineBuilder extends Builder implements SimpleBuildSt
         listener.getLogger().println("\t--Looking through branch: " + branchName + " for a jenkinsfile--");
         String jenkinsFileMetadataUrl = teamProjectUrl + "/_apis/git/repositories/" + repoId + "/items?api-version=1.0&version=" + branchName + "&scopepath=/" + file;
         org.json.JSONObject obj = callGet(client, jenkinsFileMetadataUrl);
-        if(obj.has("value")) {
+        if (obj.has("value")) {
             JSONArray fileList = obj.getJSONArray("value");
             for (int k = 0; k < fileList.length(); k++) {
                 listener.getLogger().println("\tFOUND JENKINS FILE!");
                 return true;
             }
-        }
-        else{
+        } else {
             listener.getLogger().println("\tNone found");
             return false;
         }
@@ -214,15 +200,12 @@ public class MultiBranchPipelineBuilder extends Builder implements SimpleBuildSt
         List<StandardUsernamePasswordCredentials> credentials = provider.lookupCredentials(credentialType);
 
         StandardUsernamePasswordCredentials tfsCredentials = null;
-        for(StandardUsernamePasswordCredentials c : credentials)
-        {
-            if(c.getId().equals(credentialsId))
-            {
+        for (StandardUsernamePasswordCredentials c : credentials) {
+            if (c.getId().equals(credentialsId)) {
                 tfsCredentials = c;
             }
         }
-        if(tfsCredentials == null)
-        {
+        if (tfsCredentials == null) {
             throw new Exception("No TFS username found");
         }
         return tfsCredentials;
@@ -243,7 +226,7 @@ public class MultiBranchPipelineBuilder extends Builder implements SimpleBuildSt
             String teamToken = "#team#";
 
             br = new BufferedReader(reader);
-            while ((newString = br.readLine()) != null){
+            while ((newString = br.readLine()) != null) {
                 newString = newString.replaceAll(repoToken, repoName);
                 newString = newString.replaceAll(guidToken, java.util.UUID.randomUUID().toString());
                 newString = newString.replaceAll(credentialsToken, credentialsId);
@@ -253,11 +236,10 @@ public class MultiBranchPipelineBuilder extends Builder implements SimpleBuildSt
                 strTotale.append(newString);
             }
 
-        } catch ( IOException  e) {
+        } catch (IOException e) {
             e.printStackTrace();
         } // calls it
-        finally
-        {
+        finally {
             try {
                 br.close();
             } catch (IOException e) {
@@ -269,4 +251,45 @@ public class MultiBranchPipelineBuilder extends Builder implements SimpleBuildSt
         return xml;
     }
 
+    @Extension // This indicates to Jenkins that this is an implementation of an extension point.
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+
+        public DescriptorImpl() {
+            load();
+        }
+
+
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            // Indicates that this builder can be used with all kinds of project types
+            return true;
+        }
+
+        public ListBoxModel doFillCredentialsIdItems(@AncestorInPath Item context,
+                                                     @QueryParameter String remote,
+                                                     @QueryParameter String username) {
+            if (context == null && !Jenkins.getActiveInstance().hasPermission(Jenkins.ADMINISTER) ||
+                    context != null && !context.hasPermission(Item.EXTENDED_READ)) {
+                return new StandardListBoxModel().includeCurrentValue(username);
+            }
+            return new StandardListBoxModel()
+                    .includeEmptyValue()
+                    .includeMatchingAs(
+                            context instanceof Queue.Task ? Tasks.getAuthenticationOf((Queue.Task) context) : ACL.SYSTEM,
+                            context,
+                            StandardUsernameCredentials.class,
+                            URIRequirementBuilder.fromUri(remote).build(),
+                            GitClient.CREDENTIALS_MATCHER)
+                    .includeCurrentValue(username);
+        }
+
+        public String getDisplayName() {
+            return "Generate MultiBranch pipeline from TFS GIT";
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+            save();
+            return super.configure(req, formData);
+        }
+    }
 }
